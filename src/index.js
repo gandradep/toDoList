3f5407c4452b62d82d8325e0d2cd8201cef1d9ec
gandradep <eventsonido@gmail.com>
2022-09-27 17:30:37 +0100

    testing index.js with working checkbox

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 import _ from 'lodash';
 import './style.css';
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, now imported
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+const list = document.getElementById('list');
 
-  return element;
+const todos = [
+  {
+    "description": "workout",
+    "completed": false,
+    "index": 1,
+  },
+  {
+    "description": "study mandarin",
+    "completed": false,
+    "index": 2,
+  },
+  {
+    "description": "practice guitar",
+    "completed": false,
+    "index": 3,
+  },
+];
+const iterate = (arr) => {
+  arr.forEach(element => {
+    const li = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.setAttribute("type", "checkbox");
+    li.appendChild(checkbox);
+    list.appendChild(li);
+  });
 }
-document.body.appendChild(component());
+
+iterate(todos);
+
+
+// function component() {
+//   const element = document.createElement('div');
+//
+//   // Lodash, now imported
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+//   element.classList.add('hello');
+//
+//   return element;
+// }
+// document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
   entry: './src/index.js',
   devServer: {
     static: './dist',
