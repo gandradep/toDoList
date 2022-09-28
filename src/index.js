8b8f3693b7dae689d526b5ef1d65442cada0d2c5
gandradep <eventsonido@gmail.com>
2022-09-28 04:55:38 +0100

    fixing linters

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import './style.css';
 import { todos } from './modules/objectArray.js';
 import { sort } from './modules/sort.js';
 import { iterate } from './modules/iterate.js';
+
 iterate(sort(todos));
