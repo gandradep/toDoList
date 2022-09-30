2a995bdaf13428888cecf9666d1fe409d00c1f34
gandradep <eventsonido@gmail.com>
2022-09-30 09:34:46 +0100

    fixed linters

--- src/modules/clearComplete.js
+++ src/modules/clearComplete.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/prefer-default-export */
 export const clearComplete = (arr) => {
-  const filtered = arr.filter((elem) => elem.completed == false);
+  const filtered = arr.filter((elem) => elem.completed === false);
   localStorage.setItem('Task_list', JSON.stringify(filtered));
 };
