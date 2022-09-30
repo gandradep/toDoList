70ea16c03cdf291c6eb8fcd871a928ec32f8b196
gandradep <eventsonido@gmail.com>
2022-09-30 09:30:19 +0100

    fixing linters

--- src/modules/check.js
+++ src/modules/check.js
@@ -1,7 +1,7 @@
+/* eslint-disable import/prefer-default-export */
 export const checked = (element, listArr) => {
   const arrayBoxes = Array.from(document.querySelectorAll('.check'));
   const index = arrayBoxes.indexOf(element);
   listArr[index].completed = !listArr[index].completed;
-  
   localStorage.setItem('Task_list', JSON.stringify(listArr));
-}
+};
--- src/modules/clearComplete.js
+++ src/modules/clearComplete.js
@@ -1,9 +1,5 @@
-export const clearComplete = (arr) =>{
-  const filtered = arr.filter((elem) => {
-    return elem.completed == false;
-  });
-
+/* eslint-disable import/prefer-default-export */
+export const clearComplete = (arr) => {
+  const filtered = arr.filter((elem) => elem.completed == false);
   localStorage.setItem('Task_list', JSON.stringify(filtered));
-  console.log(filtered);
-
-}
+};
